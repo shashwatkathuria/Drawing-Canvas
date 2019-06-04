721eb3ecb8b9f346fffb89270b1ef2797795e86f
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-06-04 06:39:41 +0530

    Added stroke thickness options through reactJS.

--- DrawingCanvas/MainCanvas/static/MainCanvas/javascript/reactComponents.js
+++ DrawingCanvas/MainCanvas/static/MainCanvas/javascript/reactComponents.js
@@ -65,29 +65,75 @@ class ColorClass extends React.Component
   }
 }
 
-const listItems = colors.map((color) =>
+const colorListItems = colors.map((color) =>
   <ColorClass color = {color.capitalize()} isDisabled={false} isFirst={colors.indexOf(color) == 0}/>
 );
 
 const disabledColorHeadingButton = <ColorClass color={""} isDisabled ={true} isFirst={false}/>;
 
 ReactDOM.render(
-  <div className="btn-group btn-group-toggle" data-toggle="buttons">{disabledColorHeadingButton}{listItems}</div>,
+  <div className="btn-group btn-group-toggle" data-toggle="buttons">{disabledColorHeadingButton}{colorListItems}</div>,
   document.getElementById('colorPicker')
 );
 
-// <label class="btn btn-secondary disabled" disabled>
-//     <input type="radio" name="colorOption" >Color
-// </label>
-// <label class="btn btn-secondary active">
-//     <input type="radio" class="colorClass" name="colorOption" value="black" checked> Black
-// </label>
-// <label class="btn btn-secondary">
-//     <input type="radio" class="colorClass" name="colorOption" value="blue" > Blue
-// </label>
-// <label class="btn btn-secondary">
-//     <input type="radio" class="colorClass" name="colorOption" value="green" > Green
-// </label>
-// <label class="btn btn-secondary">
-//     <input type="radio" class="colorClass" name="colorOption" value="red" > Red
-// </label>
+class ThicknessClass extends React.Component
+{
+  constructor(props)
+  {
+      super(props);
+      this.state = {thickness: this.props.thickness, thicknessClassName:thicknessClassName, thicknessName:thicknessName, disabled:this.props.isDisabled, isFirst:this.props.isFirst};
+
+      this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event)
+  {
+      this.setState({thickness: event.target.value});
+  }
+
+  render()
+  {
+    if (this.state.disabled === false)
+    {
+      if (this.state.isFirst === false)
+      {
+          return (
+              <label className="btn btn-secondary">
+                  {this.state.thickness}
+                  <input type="radio" className={this.state.thicknessClassName} style={{display:"none"}} name={this.state.thicknessName} value={this.state.thickness} />
+              </label>
+          );
+      }
+      else
+      {
+          return (
+              <label className="btn btn-secondary active">
+                  {this.state.thickness}
+                  <input type="radio" className={this.state.thicknessClassName} style={{display:"none"}} name={this.state.thicknessName} value={this.state.thickness} checked/>
+              </label>
+          );
+      }
+
+    }
+    else
+    {
+        return (
+          <label className="btn btn-secondary disabled" disabled>
+              Thickness
+              <input type="radio" name={this.state.thicknessName} />
+          </label>
+        );
+    }
+  }
+}
+
+const thicknessListItems = thicknesses.map((thickness) =>
+  <ThicknessClass thickness = {thickness} isDisabled={false} isFirst={thicknesses.indexOf(thickness) == 0}/>
+);
+
+const disabledThicknessHeadingButton = <ThicknessClass thickness={""} isDisabled ={true} isFirst={false}/>;
+
+ReactDOM.render(
+  <div className="btn-group btn-group-toggle" data-toggle="buttons">{disabledThicknessHeadingButton}{thicknessListItems}</div>,
+  document.getElementById('thicknessPicker')
+);
