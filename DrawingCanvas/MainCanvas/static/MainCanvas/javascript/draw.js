dbacd986207f5af6dbb838d4f0ab15c9089d24ba
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-06-08 09:17:31 +0530

    Separated removed points and lines array and implemented dynamic window resize by event handler.

--- DrawingCanvas/MainCanvas/static/MainCanvas/javascript/draw.js
+++ DrawingCanvas/MainCanvas/static/MainCanvas/javascript/draw.js
@@ -1,4 +1,3 @@
-// 'use strict';
 document.addEventListener('ReactDOMLoaded', () => {
 
     let isDrawing = false;
@@ -6,11 +5,16 @@ document.addEventListener('ReactDOMLoaded', () => {
     let points = [];
     let lines = [];
     let removedPointsandLines = [];
+    let removedPoints = []
+    let removedLines = []
     let svg = null;
 
-    svg = d3.select('#canvas')
-            .attr('height', window.innerHeight)
-            .attr('width', window.innerWidth);
+    var canvas = document.getElementById("canvas");
+    svg = d3.select('#canvas');
+            // .attr('height', window.innerHeight)
+            // .attr('width', window.innerWidth);
+
+    console.log(svg)
 
     svg.on('mousedown', function(){
 
@@ -104,7 +108,7 @@ document.addEventListener('ReactDOMLoaded', () => {
             {
                 points[points.length - 1].remove();
                 let removedPoint = points.pop();
-                removedPointsandLines.push(removedPoint);
+                removedPoints.push(removedPoint);
             }
         }
 
@@ -114,7 +118,7 @@ document.addEventListener('ReactDOMLoaded', () => {
             {
                 lines[lines.length - 1].remove();
                 let removedLine = lines.pop();
-                removedPointsandLines.push(removedLine);
+                removedLines.push(removedLine);
             }
         }
 
@@ -122,43 +126,43 @@ document.addEventListener('ReactDOMLoaded', () => {
 
     document.getElementById('redoButton').onclick = function()
     {
-        if (removedPointsandLines.length !== 0)
+        if (removedLines.length !== 0)
         {
-            let removedElement = removedPointsandLines.pop()
-            try
-            {
-              let strokeWidth = removedElement._groups['0']['0'].attributes['stroke-width'].value
-              let strokeColor = removedElement._groups['0']['0'].style.stroke
-              let x1 = removedElement._groups['0']['0'].x1.animVal.value
-              let x2 = removedElement._groups['0']['0'].x2.animVal.value
-              let y1 = removedElement._groups['0']['0'].y1.animVal.value
-              let y2 = removedElement._groups['0']['0'].y2.animVal.value
+            let latestRemovedLine = removedLines.pop()
+            let strokeWidth = latestRemovedLine._groups['0']['0'].attributes['stroke-width'].value
+            let strokeColor = latestRemovedLine._groups['0']['0'].style.stroke
+            let x1 = latestRemovedLine._groups['0']['0'].x1.animVal.value
+            let x2 = latestRemovedLine._groups['0']['0'].x2.animVal.value
+            let y1 = latestRemovedLine._groups['0']['0'].y1.animVal.value
+            let y2 = latestRemovedLine._groups['0']['0'].y2.animVal.value
 
-              const line = svg.append('line')
-                              .attr('x1', x1)
-                              .attr('y1', y1)
-                              .attr('x2', x2)
-                              .attr('y2', y2)
-                              .attr('stroke-width', strokeWidth)
-                              .style('stroke', strokeColor);
+            const line = svg.append('line')
+                            .attr('x1', x1)
+                            .attr('y1', y1)
+                            .attr('x2', x2)
+                            .attr('y2', y2)
+                            .attr('stroke-width', strokeWidth)
+                            .style('stroke', strokeColor);
 
-                lines.push(line);
-              } catch(TypeError){
+            lines.push(line);
 
-                let cx = removedElement._groups['0']['0'].cx.animVal.value
-                let cy = removedElement._groups['0']['0'].cy.animVal.value
-                let r = removedElement._groups['0']['0'].r.animVal.value
-                let fillColor = removedElement._groups['0']['0'].style.fill
+        }
+        if (removedPoints.length !== 0)
+        {
+            let latestRemovedPoint = removedPoints.pop()
+            let cx = latestRemovedPoint._groups['0']['0'].cx.animVal.value
+            let cy = latestRemovedPoint._groups['0']['0'].cy.animVal.value
+            let r = latestRemovedPoint._groups['0']['0'].r.animVal.value
+            let fillColor = latestRemovedPoint._groups['0']['0'].style.fill
 
-                const point = svg.append('circle')
-                                 .attr('cx', cx)
-                                 .attr('cy', cy)
-                                 .attr('r', r)
-                                 .style('fill', fillColor);
+            const point = svg.append('circle')
+                             .attr('cx', cx)
+                             .attr('cy', cy)
+                             .attr('r', r)
+                             .style('fill', fillColor);
 
-                points.push(point);
-              }
-          }
+            points.push(point);
+        }
     }
 
     function colorHexValueCheck(color)
@@ -181,5 +185,19 @@ document.addEventListener('ReactDOMLoaded', () => {
         return true
 
     }
+
+    function resizeCanvas()
+    {
+
+        let height = window.innerHeight;
+        let width = window.innerWidth;
+
+        canvas.style.width = width;
+        canvas.style.height = height;
+
+    }
+
+      resizeCanvas();
+      window.addEventListener("resize", resizeCanvas);
 
 });
