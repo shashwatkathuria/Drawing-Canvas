b073dc27ba37ae0637c06de954294b5b603086d5
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-06-18 20:49:10 +0530

    Added function for saving drawing by json format post data. Models and database saving pending.

--- DrawingCanvas/MainCanvas/views.py
+++ DrawingCanvas/MainCanvas/views.py
@@ -2,11 +2,26 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+import json
 
 # Create your views here.
-# Implement backend saving of drawings through JSON Post objects through requests according to user login
+
 def index(request):
-    response = render(request, 'MainCanvas/index.html')
+    response = modifiedResponseHeaders(render(request, 'MainCanvas/index.html'))
+    return response
+
+def saveDrawing(request):
+    if request.method == "POST":
+        savedData = json.loads(request.POST['JSONData'])
+        points = savedData['points']
+        lines = savedData['lines']
+        print(points)
+        print(lines)
+
+    response = modifiedResponseHeaders(render(request, 'MainCanvas/index.html'))
+    return response
+
+def modifiedResponseHeaders(response):
     response["Cache-Control"] = "no-cache, no-store, must-revalidate"
     response["Pragma"] = "no-cache"
     response["Expires"] = "0"
