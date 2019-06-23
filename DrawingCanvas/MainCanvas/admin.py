50a1c844957b8e1d5bfa022410fd2e19a0892f57
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-06-23 13:30:46 +0530

    Registered all models in admin site.

--- DrawingCanvas/MainCanvas/admin.py
+++ DrawingCanvas/MainCanvas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Line, Circle, Drawing
 
 # Register your models here.
+
+admin.site.register(Circle)
+admin.site.register(Line)
+admin.site.register(Drawing)
