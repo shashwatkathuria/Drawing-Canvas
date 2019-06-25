0b249577d606c0187d6671065a3d6d2d080dc0c3
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-06-25 17:36:51 +0530

    Added functionality of saving drawing in models and database.

--- DrawingCanvas/MainCanvas/views.py
+++ DrawingCanvas/MainCanvas/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from .models import Circle, Line, Drawing
 import json
 
 # Create your views here.
@@ -15,8 +16,17 @@ def saveDrawing(request):
         savedData = json.loads(request.POST['JSONData'])
         points = savedData['points']
         lines = savedData['lines']
-        print(points)
-        print(lines)
+        drawing = Drawing()
+        drawing.save()
+        for point in points:
+            circle = Circle(x = point['x'], y = point['y'], radius = point['r'], fillColor = point['color'])
+            circle.save()
+            drawing.circles.add(circle)
+        for line in lines:
+            newLine = Line(x1 = line['x1'], y1 = line['y1'], x2 = line['x2'], y2 = line['y2'], strokeColor = line['strokeColor'], strokeWidth = line['strokeWidth'])
+            newLine.save()
+            drawing.lines.add(newLine)
+        drawing.save()
 
     response = modifiedResponseHeaders(render(request, 'MainCanvas/index.html'))
     return response
