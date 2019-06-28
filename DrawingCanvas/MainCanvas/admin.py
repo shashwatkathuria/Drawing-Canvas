c41ee0cb00647620b78ef8f1a046d806018a9b30
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-06-28 18:34:45 +0530

    Deleted Line and Circle class because of inefficiency.

--- DrawingCanvas/MainCanvas/admin.py
+++ DrawingCanvas/MainCanvas/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import Line, Circle, Drawing
+from .models import Drawing
 
 # Register your models here.
 
-admin.site.register(Circle)
-admin.site.register(Line)
 admin.site.register(Drawing)
