cd8f2d10f6648a5b42e4c60881af8e920502e71e
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-07-06 21:08:36 +0530

    Added comments and some more indentation.More comments to be added and code to be refactored.

--- DrawingCanvas/MainCanvas/static/MainCanvas/javascript/draw.js
+++ DrawingCanvas/MainCanvas/static/MainCanvas/javascript/draw.js
@@ -1,7 +1,9 @@
 document.addEventListener('ReactDOMLoaded', () => {
 
+    // To keep track of whether currently drawing or not
     let isDrawing = false;
 
+    // Variables required for storing drawing data
     let points = [];
     let lines = [];
     let removedPointsandLines = [];
@@ -9,66 +11,96 @@ document.addEventListener('ReactDOMLoaded', () => {
     let removedLines = []
     let svg = null;
 
+    // JSON Data to be populated on saving drawing
     var savedData = {
         'points' : [],
         'lines' : [],
     }
 
+    // Initializing svg element
     var canvas = document.getElementById("canvas");
     svg = d3.select('#canvas');
-            // .attr('height', window.innerHeight)
-            // .attr('width', window.innerWidth);
 
-
+    // Drawing on mouse down
     svg.on('mousedown', function(){
 
         isDrawing = true;
         const mouseCoordinates = d3.mouse(this);
+        // Passing coordinates of mouse to draw point function
+        // false indicates not to be connected by line as this
+        // is the starting point, i.e., only one point
         drawPoint(mouseCoordinates[0], mouseCoordinates[1], false);
 
     });
 
+    // Not drawing on mouse up
     svg.on('mouseup', () => {
 
         isDrawing = false;
 
     });
 
+    // Continuing drawing on mouse move
     svg.on('mousemove', function(){
 
+        // Drawing only if mouse is down, i.e. isDrawing is true,
         if (!isDrawing)
+        {
             return;
+        }
 
         const mouseCoordinates = d3.mouse(this);
+        // Passing coordinates of mouse to draw point function
+        // trfue indicates line to be joined with the previous
+        // point already saved in the points array
         drawPoint(mouseCoordinates[0], mouseCoordinates[1], true);
 
     });
 
+    // Function for drawing points and lines and rendering the same
     function drawPoint(x, y, isConnect){
 
+        // Variables required for drawing
         let thickness = 0;
         let color = "";
+
+        // Getting input value of thickness from respective HTML element
         var thicknessButtons = document.getElementsByClassName("thicknessClass");
-        for (var i = 0; i < thicknessButtons.length; i++) {
+        for (var i = 0; i < thicknessButtons.length; i++)
+        {
             if(thicknessButtons[i].checked)
-              thickness = thicknessButtons[i].value;
-        }
-        var colorButtons = document.getElementsByClassName("colorClass");
-        for (var i = 0; i < colorButtons.length; i++) {
-            if(colorButtons[i].checked)
-              color = colorButtons[i].value;
+            {
+                thickness = thicknessButtons[i].value;
+            }
         }
 
+        // Getting input value of color from respective HTML element
+        var colorButtons = document.getElementsByClassName("colorClass");
+        for (var i = 0; i < colorButtons.length; i++)
+        {
+            if(colorButtons[i].checked)
+            {
+                color = colorButtons[i].value;
+            }
+        }
+
+        // Getting color hex value if any is given else color remains
+        // the one from above color class
         var colorHexValue = document.getElementById("colorHexValue");
         let tempColor = colorHexValue.value;
         if (tempColor !== "" )
+        {
             color = tempColor;
+        }
 
-
-
+        // Drawing lines if isConnect is true, i.e., the corrdinates passed
+        // are any coordinate except the first so as to be able to draw a line
         if (isConnect)
         {
+            // Getting previous point
             const previousPoint = points[points.length - 1];
+
+            // Rendering new line connecting current point to previous
             const line = svg.append('line')
                             .attr('x1', previousPoint.attr('cx'))
                             .attr('y1', previousPoint.attr('cy'))
@@ -77,9 +109,11 @@ document.addEventListener('ReactDOMLoaded', () => {
                             .attr('stroke-width', thickness * 2)
                             .style('stroke', color);
 
-              lines.push(line);
+            // Saving the new line in lines array
+            lines.push(line);
         }
 
+        // Drawing current point
         const point = svg.append('circle')
                          .attr('cx', x)
                          .attr('cy', y)
@@ -90,10 +124,15 @@ document.addEventListener('ReactDOMLoaded', () => {
 
     };
 
+    // Function to erase on clicking erase button
     document.getElementById('eraseButton').onclick = function ()
     {
+        // Removing all points and lines
+
         for (let i = 0; i < points.length; i++)
         {
+            // Removing current iteration point and saving
+            // it in removed points for future access
             points[i].remove();
             let removedPoint = points[i]
             removedPoints.push(removedPoint)
@@ -102,6 +141,8 @@ document.addEventListener('ReactDOMLoaded', () => {
 
         for (let i = 0; i < lines.length; i++)
         {
+            // Removing current iteration line and saving
+            // it in removed points for future access
             lines[i].remove();
             let removedLine = lines[i]
             removedLines.push(removedLine)
@@ -109,36 +150,62 @@ document.addEventListener('ReactDOMLoaded', () => {
 
     }
 
+    // Function to undo on clicking undo button
     document.getElementById('undoButton').onclick = function ()
     {
 
+        // Undoing last two points
         if (points.length !== 0)
         {
-            for ( let i = 0; i < 2; i++)
+            // Try catch for the case of only 1 point existing
+            try
             {
-                points[points.length - 1].remove();
-                let removedPoint = points.pop();
-                removedPoints.push(removedPoint);
+                for ( let i = 0; i < 2; i++)
+                {
+                    points[points.length - 1].remove();
+                    let removedPoint = points.pop();
+                    removedPoints.push(removedPoint);
+                }
+            }
+            catch(error)
+            {
+                continue
             }
         }
 
+        // Undoing last two lines
         if (lines.length !== 0)
         {
-            for (let i = 0; i < 2; i++)
+            // Try catch for the case of only 1 line existing
+            try
             {
-                lines[lines.length - 1].remove();
-                let removedLine = lines.pop();
-                removedLines.push(removedLine);
+                for (let i = 0; i < 2; i++)
+                {
+                    lines[lines.length - 1].remove();
+                    let removedLine = lines.pop();
+                    removedLines.push(removedLine);
+                }
             }
+            catch(error)
+            {
+                continue
+            }
+
         }
 
     }
 
+    // Function to redo on clicking redo button
     document.getElementById('redoButton').onclick = function()
     {
+        // Redoing last line if removed lines exist
         if (removedLines.length !== 0)
         {
+            // Getting last removed line and also removing it
+            // from removed lines simultaneously
             let latestRemovedLine = removedLines.pop()
+
+            // Extracting its information
             let strokeWidth = latestRemovedLine._groups['0']['0'].attributes['stroke-width'].value
             let strokeColor = latestRemovedLine._groups['0']['0'].style.stroke
             let x1 = latestRemovedLine._groups['0']['0'].x1.animVal.value
@@ -146,6 +213,7 @@ document.addEventListener('ReactDOMLoaded', () => {
             let y1 = latestRemovedLine._groups['0']['0'].y1.animVal.value
             let y2 = latestRemovedLine._groups['0']['0'].y2.animVal.value
 
+            // Drawing it
             const line = svg.append('line')
                             .attr('x1', x1)
                             .attr('y1', y1)
@@ -154,60 +222,85 @@ document.addEventListener('ReactDOMLoaded', () => {
                             .attr('stroke-width', strokeWidth)
                             .style('stroke', strokeColor);
 
+            // Pushing the new redo-ed line back into lines array
             lines.push(line);
 
         }
+
+        // Redoing last point if removed points exist
         if (removedPoints.length !== 0)
         {
+            // Getting last removed point and also removing it
+            // from removed points simultaneously
             let latestRemovedPoint = removedPoints.pop()
             let cx = latestRemovedPoint._groups['0']['0'].cx.animVal.value
             let cy = latestRemovedPoint._groups['0']['0'].cy.animVal.value
             let r = latestRemovedPoint._groups['0']['0'].r.animVal.value
             let fillColor = latestRemovedPoint._groups['0']['0'].style.fill
 
+            // Drawing it
             const point = svg.append('circle')
                              .attr('cx', cx)
                              .attr('cy', cy)
                              .attr('r', r)
                              .style('fill', fillColor);
 
+            // Pushing the new redo-ed point back into points array
             points.push(point);
         }
     }
 
+    // Function to check the format of the input of hex color value
     function colorHexValueCheck(color)
     {
+        // Hex element values array
         let hexValues = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
 
+        // Hex colors are 6 hex digits long
         if (color.length !== 6)
-            return false
-
-        for(var k = 0; k < color.length ; k++)
         {
+            return false
+        }
 
+        // For loop to check each element
+        for (let k = 0; k < color.length ; k++)
+        {
+            // Continuing to next iteration if correct
             if (color[k] in hexValues)
+            {
                 continue
+            }
+            // Else returning false
             else
+            {
                 return false
+            }
 
         }
 
+        // Returning true in the end if everything is correct
         return true
 
     }
 
+    // Function to resize the canvas
     function resizeCanvas()
     {
 
+        // Getting window dimensions
         let height = window.innerHeight;
         let width = window.innerWidth;
 
+        // Drawing on the canvas
         canvas.style.width = width;
         canvas.style.height = height;
 
     }
 
+    // Resizing canvas in the starting of program
     resizeCanvas();
+
+    // Resizing canvas if resize event is triggered at any point of timse
     window.addEventListener("resize", resizeCanvas);
 
     document.getElementById('saveDrawingButton').onclick = function ()
